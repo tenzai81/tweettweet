cd81b956c71827c1fac1c89e66cf8fac0515ade0
Huy Nguyen <tenzai81@gmail.com>
2019-10-24 22:53:16 -0500

    dddd

--- config/config.js
+++ config/config.js
@@ -5,11 +5,11 @@ module.exports= {
     database: "jryulqlv",
     host: "salt.db.elephantsql.com",
     dialect: "postgres"  },
-//   production: {
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOSTNAME,
-//     dialect: "postgres"  }
-//  
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres"  }
+ 
 }
